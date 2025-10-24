653ddaeffa088fb1da0b234a25eaf3a72efd575f
mstoyanovca <mstoyanovca@gmail.com>
2025-10-24 18:24:56 -0400

    created async module

---
+++ spring/async/src/test/java/async/AsyncApplicationTests.java
@@ -0,0 +1,11 @@
+package async;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
