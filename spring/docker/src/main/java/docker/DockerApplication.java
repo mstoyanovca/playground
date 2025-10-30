f21e5f116bd205eff6336743f29ff5a22833b2da
mstoyanovca <mstoyanovca@gmail.com>
2025-10-29 20:30:03 -0400

    created module docker

---
+++ spring/docker/src/main/java/docker/DockerApplication.java
@@ -0,0 +1,11 @@
+package docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerApplication {
+    static void main(String[] args) {
+        SpringApplication.run(DockerApplication.class, args);
+    }
+}
