04c7817de2caadf3b7391a77cfa55410a1dc8bfe
mstoyanovca <mstoyanovca@gmail.com>
2025-11-01 20:32:14 -0400

    added DAO layer

---
+++ spring/reactive/src/main/java/reactive/controller/CustomerController.java
@@ -0,0 +1,44 @@
+package reactive.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactive.entity.Customer;
+import reactive.repository.CustomerRepository;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    // this is what would be coming from an Angular front end:
+    /*@PostMapping(path = "/create")
+    public Customer create(@RequestBody Customer customer) {
+        return customer;
+    }*/
+
+    @GetMapping(path = "/create")
+    public Customer create(@RequestParam String firstName, @RequestParam String lastName) {
+        // http://localhost:8080/customer/create?firstName=Martin&lastName=Stoyanov
+        Customer customer = new Customer();
+        customer.setFirstName(firstName);
+        customer.setLastName(lastName);
+        Customer savedCustomer = customerRepository.save(customer);
+        customer.setId(savedCustomer.getId());
+        return customer;
+    }
+
+    @GetMapping(path = "/all")
+    public List<Customer> findAll() {
+        return customerRepository.findAll();
+    }
+}
--- spring/reactive/src/main/java/reactive/repository/CustomerRepository.java
+++ spring/reactive/src/main/java/reactive/repository/CustomerRepository.java
@@ -1,8 +1,8 @@
 package reactive.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import reactive.entity.Customer;
 
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
     // Spring Data JPA creates a customerRepository implementation, when you run the application;
 }
