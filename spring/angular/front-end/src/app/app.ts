111fd76504fe87a3ef286af32b91885ef27d348f
mstoyanovca <mstoyanovca@gmail.com>
2025-11-09 18:05:49 -0500

    updated front-end

--- spring/angular/front-end/src/app/app.ts
+++ spring/angular/front-end/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('front-end');
+  protected readonly title = signal('login');
 }
