864d31e91527d7a282b973bc79d01a644239f671
mstoyanovca <mstoyanovca@gmail.com>
2025-11-08 19:59:07 -0500

    created CustomerServiceTest

--- spring/reactive/src/main/java/reactive/service/CustomerService.java
+++ spring/reactive/src/main/java/reactive/service/CustomerService.java
@@ -4,7 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.reactive.TransactionalOperator;
+import org.springframework.transaction.annotation.Transactional;
 import reactive.entity.Customer;
 import reactive.repository.CustomerRepository;
 import reactor.core.publisher.Flux;
@@ -15,17 +15,16 @@ import java.util.stream.IntStream;
 @Service
 public class CustomerService {
     private final CustomerRepository customerRepository;
-    private final TransactionalOperator transactionalOperator;
 
     private static final int QUANTITY = 1000;
     private static final Logger LOGGER = LoggerFactory.getLogger(CustomerService.class);
 
     @Autowired
-    public CustomerService(CustomerRepository customerRepository, TransactionalOperator transactionalOperator) {
+    public CustomerService(CustomerRepository customerRepository) {
         this.customerRepository = customerRepository;
-        this.transactionalOperator = transactionalOperator;
     }
 
+    @Transactional
     public Flux<Customer> createCustomers() {
         LOGGER.info("createCustomers()");
 
@@ -38,7 +37,6 @@ public class CustomerService {
         return customerRepository
                 .deleteAll()
                 .thenMany(customerRepository.saveAll(customers))
-                .thenMany(customerRepository.findAll())
-                .as(transactionalOperator::transactional);
+                .thenMany(customerRepository.findAll());
     }
 }
